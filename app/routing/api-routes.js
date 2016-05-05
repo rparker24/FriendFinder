9a28030e6668d71271822bc2340fa12fa1edf977
rparker24 <robert@robertmparker.net>
2016-05-05 00:43:59 -0400

    fixed  html routes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,11 +1,24 @@
-
-
+var express = require('express');
 var path = require('path');
 
-app.get('/api/friends', function (req, res) {
-	res.json(friends);
-});
+var app = express();
 
-app.post('/api/friends', function(req, res) {
-	res.json(newFriend);
-});
+var friendsData	= require('../data/friends.js');
+
+module.exports = function(app){
+
+	app.get('/api/friends', function(req, res) {
+		res.json(friendsData);
+	});
+
+	app.post('/api/friends', function(req, res) {
+		res.json(newFriend);
+	});
+
+	app.post('/api/clear', function(req, res) {
+		// Empty out the arrays of data
+		friendsData = [];
+
+		console.log(friendsData);
+	})
+}
