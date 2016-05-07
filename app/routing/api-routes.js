483d679d7e3b9307d0d21f610335071c815a6164
rparker24 <robert@robertmparker.net>
2016-05-07 09:16:55 -0400

    add function for calculation

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -5,6 +5,15 @@ var app = express();
 
 var friendsData	= require('../data/friends.js');
 
+function checkScores() {
+	var userTotal = 0;
+	for (var i = 0; i < friendsData.length; i++) {
+		for (var j = 0; j < friendsData[i].scores.length; j++) {
+			userTotal += friendsData[i].scores[j];
+		}
+	}
+}
+
 module.exports = function(app){
 
 	app.get('/api/friends', function(req, res) {
@@ -13,15 +22,12 @@ module.exports = function(app){
 
 	app.post('/api/friends', function(req, res) {
 		var newFriend = req.body;
-		// console.log(newFriend);
 		friendsData.push(newFriend);
 		res.json(friendsData);
+		checkScores();
 	});
 
 	app.post('/api/clear', function(req, res) {
-		// Empty out the arrays of data
 		friendsData = [];
-
-		console.log(friendsData);
 	})
 }
